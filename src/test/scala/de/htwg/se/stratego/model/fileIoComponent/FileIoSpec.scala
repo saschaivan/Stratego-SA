3602884b8cd9e24e486d696e4fec17fd1db23e57
Benjamin Mannsdörfer <b.mannsdoerfer@gmx.de>
2021-03-28 20:05:37 +0200

    fixed FileIoSpec

--- src/test/scala/de/htwg/se/stratego/model/fileIoComponent/FileIoSpec.scala
+++ src/test/scala/de/htwg/se/stratego/model/fileIoComponent/FileIoSpec.scala
@@ -12,27 +12,26 @@ class FileIoSpec extends WordSpec with Matchers {
       val characList = CharacterList(4)
       val playerBlue = Player("PlayerBlue", characList.getCharacterList())
       val playerRed = Player("PlayerRed", characList.getCharacterList())
-      val game = Game(playerBlue, playerRed, 4, matchField).init(matchField, 0, 0, 0, 0)
-      val controller = new Controller(game.matchField)
+      val game = Game(playerBlue, playerRed, 4, matchField)
+      val board = game.init(matchField, 0, 0, 0, 0)
+      val board1 = game.moveDown(board.matchField,0,0)
+      val controller = new Controller(board1)
 
       "save and load the Gamestate with Json" in {
         import de.htwg.se.stratego.model.fileIoComponent.fileIoJsonImpl.FileIO
         val fileIOJson = new FileIO()
-        fileIOJson.save(game.matchField, controller.currentPlayerIndex, List[Player](playerBlue, playerRed))
+        fileIOJson.save(board.matchField, controller.currentPlayerIndex, List[Player](playerBlue, playerRed))
         val fileIOLoadAsString = fileIOJson.load.toString()
         fileIOLoadAsString should be("(   0     1     2     3  \n+-----+-----+-----+-----+\n|  \u001B[0;34m9\u001B[0m  |  \u001B[0;34m8\u001B[0m  |  \u001B[0;34m6\u001B[0m  |  \u001B[0;34mF\u001B[0m  | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|  \u001B[0;31m9\u001B[0m  |  \u001B[0;31m8\u001B[0m  |  \u001B[0;31m6\u001B[0m  |  \u001B[0;31mF\u001B[0m  | 3\n+-----+-----+-----+-----+\n**********  STRATEGO  **********\n\nn:   create a new empty machtfield\nz:   undo\ny:   redo\nq:   quit the programm\n,0,\"PlayerBlue PlayerRed\")")
       }
 
-
       "save and load the Gamestate with XML" in {
         import de.htwg.se.stratego.model.fileIoComponent.fileIoXmlImpl.FileIO
         val fileIOXML = new FileIO()
-        fileIOXML.save(game.matchField, controller.currentPlayerIndex, List[Player](playerBlue, playerRed))
+        fileIOXML.save(board.matchField, controller.currentPlayerIndex, List[Player](playerBlue, playerRed))
         val fileIOLoadAsString = fileIOXML.load.toString()
         fileIOLoadAsString should be("(   0     1     2     3  \n+-----+-----+-----+-----+\n|  \u001B[0;34m9\u001B[0m  |  \u001B[0;34m8\u001B[0m  |  \u001B[0;34m6\u001B[0m  |  \u001B[0;34mF\u001B[0m  | 0\n+-----+-----+-----+-----+\n|     |     |     |     | 1\n+-----+-----+-----+-----+\n|     |     |     |     | 2\n+-----+-----+-----+-----+\n|  \u001B[0;31m9\u001B[0m  |  \u001B[0;31m8\u001B[0m  |  \u001B[0;31m6\u001B[0m  |  \u001B[0;31mF\u001B[0m  | 3\n+-----+-----+-----+-----+\n**********  STRATEGO  **********\n\nn:   create a new empty machtfield\nz:   undo\ny:   redo\nq:   quit the programm\n,0,PlayerBlue PlayerRed)")
       }
-
-
     }
   }
 }
